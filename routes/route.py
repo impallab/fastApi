e48f8f3edccf66b02e910045e1977a73ef8bcd86
impallab <pallab13nayagram@gmail.com>
2024-03-04 22:16:55 +0530

    Added the create, update and delete route for the API

--- routes/route.py
+++ routes/route.py
@@ -6,9 +6,23 @@ from bson import ObjectId
 
 router =APIRouter()
 
+# Create Asset :
+@router.post("/")
+async def Add_an_asset(asset:Asset):
+    collection.insert_one(dict(asset))
+
 #Read Assets :
 @router.get("/")
 async def Get_all_assets():
     assets = list_serial(collection.find())
     return assets
 
+#Update Asset :
+@router.put("/{id}")
+async def  Update_asset(id:str, asset:Asset):
+    collection.find_one_and_update( {"_id":ObjectId(id)},{"$set": dict(asset)})
+
+#Delete Asset :
+@router.delete("/{id}")
+async def Delete_Asset(id:str):
+    collection.find_one_and_delete({"_id":ObjectId(id)})
